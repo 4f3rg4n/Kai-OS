cbb7c823fb8b29e1f7d45c994809362cc8edd88d
AI-fergan <ifrganvrcturh@gmail.com>
2024-04-22 14:09:56 +0300

    Added print function that can print strings

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -1,37 +1,39 @@
 [BITS 16]
 [ORG 0x7c00]
 
-mov ah, 0x0e
-mov al, 'N'
-int 0x10
-mov al, 'A'
-int 0x10
-mov al, 'p'
-int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'n'
-int 0x10
-mov al, 'g'
-int 0x10
-mov al, 'u'
-int 0x10
-mov al, 'i'
-int 0x10
-mov al, 'n'
-int 0x10
-mov al, '-'
-int 0x10
-mov al, 'O'
-int 0x10
-mov al, 'S'
-int 0x10
-mov al, '.'
-int 0x10
-int 0x10
-int 0x10
+;print open screen message
+push open_message
+call print
+pop ax
 
 jmp $
+
+;This function get string pointer and print it
+print:
+    push si
+    push bp
+    mov bp, sp
+
+
+    mov ah, 0x0e ; tty
+    mov si, [bp + 6]
+
+    print_flow:
+        mov al, [si]
+        cmp al, 0
+        je stop_print                
+        int 0x10
+        inc si
+        jmp print_flow
+
+    stop_print:
+        mov sp, bp
+        pop bp
+        pop si
+        ret 
+
+open_message:
+    db 'NApenguin-OS', 0
 
 TIMES 510-($-$$) db 0
 dw 0xaa55
