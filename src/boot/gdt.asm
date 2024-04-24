45359f33610da79428381f0cba72c6006330102b
AI-fergan <ifrganvrcturh@gmail.com>
2024-04-24 12:41:06 +0300

    Added GDT null, code, data descriptors

---
+++ src/boot/gdt.asm
@@ -0,0 +1,54 @@
+;GDT - Global Descriptor
+GDT_start:
+
+    ;This descriptor is empty and has only null bytes
+    null_descriptor:
+        dd 0
+        dd 0
+
+    ;This descriptor use for code
+    code_descriptor:
+        dw 0xffff    ;Descriptor limit size
+
+        dw 0         ;Start of descriptor address 3-bytes
+        db 0           
+                     ; Flags settings:
+                     ; ---------------------------------------------------------------------------------------------------------------
+        db 10011010b ;|First 4 bits:  1-bit present = 1      |2-bit privilage = 00   |1-bit codedata(1) or traps(0) = 1               |
+                     ;|--------------------------------------|-----------------------|------------------------------------------------|
+                     ;|Second 4 bits: 1-bit contains code = 1|1-bit is conforming = 0|1-bit is readable = 1|Accessed = 0 (cpu bit...) |
+                     ; ---------------------------------------------------------------------------------------------------------------
+
+                     ; Flags settings:
+                     ; ------------------------------------------------------------------------------------------------------------------------------
+        db 11001111b ;|One byte: 1-bit Granularity = 1|1-bit 32 bit code = 1|1-bit is64? = 0|1-bit Userbit = 0|4-bit segment limit 4 more bits = 1111|
+                     ; ------------------------------------------------------------------------------------------------------------------------------
+
+        db 0         ;End ofdescriptor addres 1-byte
+
+    ;This descriptor use for data
+    data_descriptor:
+        dw 0xffff    ;Descriptor limit size
+
+        dw 0         ;Start of descriptor address 3-bytes
+        db 0           
+                     ; Flags settings:
+                     ; ---------------------------------------------------------------------------------------------------------------
+        db 10010010b ;|First 4 bits:  1-bit present = 1      |2-bit privilage = 00   |1-bit codedata(1) or traps(0) = 1               |
+                     ;|--------------------------------------|-----------------------|------------------------------------------------|
+                     ;|Second 4 bits: 1-bit contains code = 0|1-bit is direction = 0 |1-bit is writeable = 1|Accessed = 0 (cpu bit...)|
+                     ; ---------------------------------------------------------------------------------------------------------------
+
+                     ; Flags settings:
+                     ; ------------------------------------------------------------------------------------------------------------------------------
+        db 11001111b ;|One byte: 1-bit Granularity = 1|1-bit 32 bit code = 1|1-bit is64? = 0|1-bit Userbit = 0|4-bit segment limit 4 more bits = 1111|
+                     ; ------------------------------------------------------------------------------------------------------------------------------
+
+        db 0         ;End ofdescriptor addres 1-byte
+
+GDT_end:
+
+;GDT descriptor
+GDT_descriptor:
+    dw GDT_start - GDT_end - 1 ;GDT size
+    dd GDT_start               ;GDT address
