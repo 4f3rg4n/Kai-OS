24a826e9b3497e5909b5206f1e435cdb80b070da
AI-fergan <ifrganvrcturh@gmail.com>
2024-04-24 09:42:23 +0300

    Added enter to new_line function

--- src/boot/helper.asm
+++ src/boot/helper.asm
@@ -4,9 +4,10 @@ print:
     push bp
     mov bp, sp
 
-    mov ah, 0x0e ; tty
+    mov ah, 0x0E ;tty
     mov si, [bp + 6]
 
+    ;print until detect null byte
     print_flow:
         mov al, [si]
         cmp al, 0
@@ -15,9 +16,17 @@ print:
         inc si
         jmp print_flow
 
+    ;when null byte
     stop_print:
         mov sp, bp
         pop bp
         pop si
         ret 
                 
+;This function enter to new write line                
+new_line:
+    mov ah, 0x0E ;tty
+    mov al, 0x0A ;enter
+    int 0x10
+    mov al, 0x0D ;back to start of line
+    int 0x10
