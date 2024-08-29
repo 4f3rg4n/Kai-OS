494632ce6e8f612a63a8f950e558e1b5cbd64db8
AI-fergan <ifrganvrcturh@gmail.com>
2024-08-29 16:53:34 +0300

    Added idt set new gate, idt init and load functions

--- kernel/cpu/include/idt.h
+++ kernel/cpu/include/idt.h
@@ -3,27 +3,29 @@
 
 #include "../../libs/include/types.h"
 
-struct idt_entry {
+#define IDT_ENTRYS 256
+
+typedef struct idt_entry {
     u16bit base_low;    
     u16bit sel;         
-    u8bit  zero;     
+    u8bit  all_zero;     
     u8bit  flags;       
     u16bit base_high;   
-} __attribute__((packed)) /*no padding*/; 
+} idt_entry __attribute__((packed)) /*no padding*/; 
 
-struct idt_ptr {
+typedef struct idt_ptr {
     u16bit limit;       
     u32bit base;          
-} __attribute__((packed)) /*no padding*/;
+} idt_ptr __attribute__((packed)) /*no padding*/;
 
 //idt
-struct idt_entry idt[256];
+idt_entry idt[IDT_ENTRYS];
 
 //idt pointer
-struct idt_ptr idtp;
+idt_ptr idtr;
 
 //idt helper functions
-void set_new_gate(u8bit num, u32bit base, u16bit sel, u8bit flags);
+void set_new_idt_gate(u8bit num, u32bit base, u16bit sel, u8bit flags);
 void idt_init();
 void idt_load();
 
--- kernel/cpu/src/idt.c
+++ kernel/cpu/src/idt.c
@@ -1,13 +1,19 @@
 #include "../include/idt.h"
 
-void set_new_gate(u8bit num, u32bit base, u16bit sel, u8bit flags){
-
+void set_new_idt_gate(u8bit num, u32bit base, u16bit sel, u8bit flags) {
+    idt[num].base_low = (base & 0xFFFF);        //low 16 bits
+    idt[num].base_high = (base >> 16) & 0xFFFF; //high 16 bits
+    idt[num].sel = sel;                         
+    idt[num].all_zero = 0;                      
+    idt[num].flags = flags; 
 }
 
-void idt_init(){
-
+void idt_init() {
+    idtr.limit = (sizeof(idt_entry) * IDT_ENTRYS) - 1;
+    idtr.base = (u32bit)&idt;
+    idt_load();
 }
 
-void idt_load(){
-
+void idt_load() {
+    __asm__ __volatile__("lidtl (%0)" : : "r" (&idtr));
 }
