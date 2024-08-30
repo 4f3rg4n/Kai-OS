8dffbf7451ef521cc94678abde2d766e20c4be09
AI-fergan <ifrganvrcturh@gmail.com>
2024-08-30 11:42:08 +0300

    Added 22 isr functions

--- kernel/cpu/include/isr.h
+++ kernel/cpu/include/isr.h
@@ -9,6 +9,27 @@ void isr_init();
 void isr_handler(u16bit entry);
 
 extern void isr0();  
-extern void isr1();
+extern void isr1();  
+extern void isr2();
+extern void isr3();  
+extern void isr4();
+extern void isr5();  
+extern void isr6();
+extern void isr7();  
+extern void isr8();
+extern void isr9();  
+extern void isr10();
+extern void isr11();  
+extern void isr12();
+extern void isr13();  
+extern void isr14();
+extern void isr15();  
+extern void isr16();
+extern void isr17();  
+extern void isr18();
+extern void isr19();  
+extern void isr20();
+extern void isr21();  
+extern void isr22();
 
 #endif
--- kernel/cpu/src/isr.c
+++ kernel/cpu/src/isr.c
@@ -28,6 +28,27 @@ const char *isr_desc[] = {
 void isr_init() {
     idt_set_new_gate(0, (u32bit)isr0, 0x08, 0x8E);
     idt_set_new_gate(1, (u32bit)isr1, 0x08, 0x8E);
+    idt_set_new_gate(2, (u32bit)isr2, 0x08, 0x8E);
+    idt_set_new_gate(3, (u32bit)isr3, 0x08, 0x8E);
+    idt_set_new_gate(4, (u32bit)isr4, 0x08, 0x8E);
+    idt_set_new_gate(5, (u32bit)isr5, 0x08, 0x8E);
+    idt_set_new_gate(6, (u32bit)isr6, 0x08, 0x8E);
+    idt_set_new_gate(7, (u32bit)isr7, 0x08, 0x8E);
+    idt_set_new_gate(8, (u32bit)isr8, 0x08, 0x8E);
+    idt_set_new_gate(9, (u32bit)isr9, 0x08, 0x8E);
+    idt_set_new_gate(10, (u32bit)isr10, 0x08, 0x8E);
+    idt_set_new_gate(11, (u32bit)isr11, 0x08, 0x8E);
+    idt_set_new_gate(12, (u32bit)isr12, 0x08, 0x8E);
+    idt_set_new_gate(13, (u32bit)isr13, 0x08, 0x8E);
+    idt_set_new_gate(14, (u32bit)isr14, 0x08, 0x8E);
+    idt_set_new_gate(15, (u32bit)isr15, 0x08, 0x8E);
+    idt_set_new_gate(16, (u32bit)isr16, 0x08, 0x8E);
+    idt_set_new_gate(17, (u32bit)isr17, 0x08, 0x8E);
+    idt_set_new_gate(18, (u32bit)isr18, 0x08, 0x8E);
+    idt_set_new_gate(19, (u32bit)isr19, 0x08, 0x8E);
+    idt_set_new_gate(20, (u32bit)isr20, 0x08, 0x8E);
+    idt_set_new_gate(21, (u32bit)isr21, 0x08, 0x8E);
+    idt_set_new_gate(22, (u32bit)isr22, 0x08, 0x8E);
 }
 
 void isr_handler(u16bit entry) {
