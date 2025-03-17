985a0c7a7fada317793e5d8f27ddf23bcc3faf07
AI-fergan <ifrganvrcturh@gmail.com>
2025-03-17 19:10:57 +0200

    Added print idx

--- boot/helper_32bit.asm
+++ boot/helper_32bit.asm
@@ -7,24 +7,28 @@ print_32bit:
 
     mov ah , 0x0f
     mov edx , 0xb8000
+    add edx, [print_idx32bit]
 
     ;print until detect null byte
     print_flow_32bit:
         mov al , byte [ebx]
+        
         cmp al , 0 
         je stop_print_32bit
 
         mov [edx] , ax
-        
+
         inc ebx
+        add dword [print_idx32bit], 2
         add edx , 2 
         jmp print_flow_32bit
     
     ;when null byte
     stop_print_32bit:
-
     mov esp, ebp
     pop ebp
     pop edx
     pop ebx
     ret
+
+print_idx32bit dd 0
